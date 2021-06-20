b39c2feb860adcddeb7c9af216a85bfd6e943216
lavsam-dev <lavsam@gmail.com>
2021-06-20 19:13:33 +0400

    Notes8 v0.1.1

--- app/src/main/java/com/geekbrains/lavsam/notes8/domain/Note.java
+++ app/src/main/java/com/geekbrains/lavsam/notes8/domain/Note.java
@@ -7,7 +7,6 @@ public class Note implements Parcelable {
 
     private final String id;
     private final String title;
-
     private final String url;
 
     public Note(String id, String title, String url) {
